c2c06d3f630bef1dde884f8ad36a23354334f837
Lopatorr <roman.lakusta@kpk-lp.com.ua>
2022-03-27 20:52:48 +0300

    gorshok

---
+++ main1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout<<"Statement about yourself " <<endl;
+	cout<<"Name:Lakusta Roman Sergiyovych\n";
+	cout<<"Date of birthday:02.12.2004\n";
+	cout<<"Place of residence:Kolomyya\n";
+	cout<<"Number of native family members:5\n"<<endl<<endl;
+	cout<<"Schedule of couples for Monday:"<<endl;
+	cout<<"1 Para;There is not\n";
+	cout<<"2 Para:History\n";
+	cout<<"3 Para;Ukrainian language\n";
+	cout<<"4 Para:Math\n"<<endl<<endl;
+	cout<<"Drawing symbols:"<<endl<<endl;
+cout<<"  (\__/) \n";
+cout<<"  (='.'=)\n";
+cout<<"  E[:]|||||[:]? \n";
+cout<<"  (-)_(-) \n"<<endl<<endl;
+
+cout<<"Praktychna robota 2. P-21 Variant 11" <<endl<<endl;
+
+cout<<sizeof(int)<<endl;
+cout<<sizeof(float)<<endl;
+cout<<sizeof(char)<<endl;
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+}
